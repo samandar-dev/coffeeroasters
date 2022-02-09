5c0423b1f84050d2d6fc652f4d8d67f8ee171fb1
samandar-dev <samandarbohodirov01@gmail.com>
2022-02-09 13:55:22 +0500

    creat

--- js/main.js
+++ js/main.js
@@ -3,17 +3,15 @@ let elMenuBox = document.getElementById('menuBox');
 let elMenuRemov = document.getElementById('menuRemov')
 
 elMenuBtn.addEventListener('click', function () {
+  elMenuBox.classList.add('open-menu');
   elMenuBtn.classList.add('m-none');
+  elMenuBtn.classList.remove('m-block');
+  elMenuRemov.classList.add('m-block');
 })
 
 elMenuRemov.addEventListener('click', function () {
   elMenuBtn.classList.add('m-block');
-})
+  elMenuBox.classList.remove('open-menu');
+  elMenuRemov.classList.add('m-none');
 
-elMenuBtn.addEventListener('click', function () {
-  elMenuBox.classList.add('open-menu');
-})
-
-elMenuRemov.addEventListener('click', function () {
-  elMenuBox.classList.add('menu-remov');
 })
